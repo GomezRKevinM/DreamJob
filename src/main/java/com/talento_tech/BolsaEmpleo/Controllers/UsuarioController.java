5d8d943293120bd046ad844b02b5581f117ff666
KevinGr <kgomezr2@unicartagena.edu.co>
2025-06-18 21:01:28 -0500

    agregando swagger

--- src/main/java/com/talento_tech/BolsaEmpleo/Controllers/UsuarioController.java
+++ src/main/java/com/talento_tech/BolsaEmpleo/Controllers/UsuarioController.java
@@ -11,8 +11,12 @@ import com.talento_tech.BolsaEmpleo.Services.ServiceUsuario;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Usuario Controller", description = "Controlador para manejar las operaciones de usuario")
 public class UsuarioController {
     ServiceUsuario serviceUsuario;
 
@@ -21,12 +25,15 @@ public class UsuarioController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "Agregar un nuevo usuario")    
     public void agregar(@RequestBody Usuario usuario) {
         serviceUsuario.agregarUsuario(usuario);
     }
 
     @GetMapping("/user/{id}")
     @ResponseBody
+    @Operation(summary = "Obtener un usuario por ID")
+    @CrossOrigin(origins = "*") // Permitir solicitudes desde cualquier origen
     public Usuario getUsuarioById(@PathVariable Long id) {
         if (id == null || id <= 0) {
             throw new IllegalArgumentException("El ID del usuario debe ser un número positivo.");
@@ -36,11 +43,13 @@ public class UsuarioController {
     }
 
     @PutMapping("/edit")
+    @Operation(summary = "Editar un usuario existente")
     public void editar(@RequestBody Usuario usuario) {
         serviceUsuario.editarUsuario(usuario);
     }
 
     @DeleteMapping("/del/{id}")
+    @Operation(summary = "Eliminar un usuario por ID")  
     public void eliminar(@PathVariable Long id) {
         if (id == null || id <= 0) {
             throw new IllegalArgumentException("El ID del usuario debe ser un número positivo.");
@@ -50,11 +59,15 @@ public class UsuarioController {
 
     @GetMapping("/list")
     @ResponseBody
+    @Operation(summary = "Listar todos los usuarios")
+    @CrossOrigin(origins = "*") // Permitir solicitudes desde cualquier origen
     public ArrayList<Usuario> listar(HttpServletRequest request){
         return serviceUsuario.getAllUsers(request);
     }
 
     @GetMapping("/count")
+    @ResponseBody
+    @Operation(summary = "Contar el número de usuarios")
     public Integer contar(HttpServletRequest request){
         if(listar(request) == null) {
             return 0; // Si la lista es nula, retornar 0
