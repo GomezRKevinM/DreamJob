06a5763469841829fb7d6a0ab46d029418098a1a
KevinGr <kgomezr2@unicartagena.edu.co>
2025-07-14 15:58:12 -0500

    feat: Kevin gomez - Controlador de empesa Listo

--- src/main/java/com/talento_tech/BolsaEmpleo/Controllers/EmpresaController.java
+++ src/main/java/com/talento_tech/BolsaEmpleo/Controllers/EmpresaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.SQLException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,6 +16,8 @@ import com.talento_tech.BolsaEmpleo.dto.ResponseDto;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -53,6 +56,41 @@ public class EmpresaController {
         }
     }
     
+    @PostMapping("/edit")
+    public ResponseEntity<ResponseDto> editar(@RequestBody Empresa entity) {
+        Empresa saved;
+        try {
+            saved = Empresa.class.cast(serviceEmpresa.editarEmpresa(entity).getData());
+            if(saved != null) {
+                return ResponseEntity.status(201).body(new ResponseDto("Empresa editada exitosamente", saved, 201));
+            } else {
+                return ResponseEntity.status(500).body(new ResponseDto("Error al editar la empresa", null, 500));
+            }
+        } catch (SQLException e) {
+            return ResponseEntity.status(500).body(new ResponseDto("Error al editar la empresa", e, 500));
+        }
+    }
+    
+    @DeleteMapping("/del/{id}")
+    public ResponseEntity<ResponseDto> eliminar(@PathVariable Long id) {
+        Empresa saved;
+        try {
+            saved = Empresa.class.cast(serviceEmpresa.eliminarEmpresa(id).getData());
+            if(saved != null) {
+                return ResponseEntity.status(200).body(new ResponseDto("Empresa eliminada exitosamente", saved, 200));
+            } else {
+                return ResponseEntity.status(500).body(new ResponseDto("Error al eliminar la empresa", null, 500));
+            }
+        } catch (SQLException e) {
+            return ResponseEntity.status(500).body(new ResponseDto("Error al eliminar la empresa", e, 500));
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ResponseDto> getList() {
+        ResponseDto response = serviceEmpresa.getEmpresas();
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
     
 
 }
